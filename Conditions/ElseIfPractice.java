d79b0c50ff9343158fdedf4895f73dc3fdf19294
sagar7513 <sagargupta7513@gmail.com>
2025-10-27 13:28:13 +0530

    conditions practice codes

---
+++ Conditions/ElseIfPractice.java
@@ -0,0 +1,15 @@
+public class ElseIfPractice{
+	public static void main(String[] args){
+		int code=0;
+		if(code==0){
+			System.out.println("it is a 0");
+		}
+		else if (code>0){
+			System.out.println("it is positive number");
+			
+		}
+		else{
+			System.out.println("it is negative number");
+		}
+	}
+}
---
+++ Conditions/IfElsePractice.java
@@ -0,0 +1,17 @@
+public class IfElsePractice{
+	public static void main(String[] args){
+		int code = 25;
+		if (code<20){
+			System.out.println("less than 20");
+		}
+		else{
+			System.out.println("more than 20");
+		}
+		if(code%2==0){
+			System.out.println("it is even number");
+		}
+		else{
+			System.out.println("it is odd number");
+		}
+	}
+}
